5a014b198868a1711e5fd10a34711c887a51135a
Va-Sophie <107587035+Va-Sophie@users.noreply.github.com>
2022-08-02 21:37:30 +0200

    wind speed

--- src/index.js
+++ src/index.js
@@ -32,8 +32,13 @@ function humidity(element) {
   let humidity = element.data.main.humidity;
   document.getElementById(`humidity`).innerHTML = `${humidity}%`;
 }
+function windSpeed(element) {
+  let speed = Math.round(element.data.wind.speed);
+  document.getElementById("windSpeed").innerHTML = `${speed} km/h`;
+}
 let apiKey = "0b3a182635d594d7bd1abd38c840f9c3";
 let units = "metric";
 let ApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=${apiKey}&units=${units}`;
 axios.get(`${ApiUrl}`).then(showTemperature);
 axios.get(`${ApiUrl}`).then(humidity);
+axios.get(`${ApiUrl}`).then(windSpeed);
